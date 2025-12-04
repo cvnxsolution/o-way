ea6c51beabbb32b9e6688c8d2e6644fa1f05703b
cvnxsolution <cvnxsolution@gmail.com>
2025-12-04 22:14:40 +0630

    initial commit

---
+++ src/main/java/org/cvnx/oway/OwayApplication.java
@@ -0,0 +1,13 @@
+package org.cvnx.oway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cvnx/oway/OwayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cvnx.oway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
